d0f5cf6084e45e38c4cd8a4ab3ad17dd74d042a1
LyyHua <lyhuavanly@gmail.com>
2024-07-27 16:33:12 +0700

    functional and workable searchbar which surprise me since nothing broke FOR THE MOMENT

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
-import { Searchbar } from "react-native-paper";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 import {
   Loading,
   LoadingContainer,
   RestaurantList,
-  SearchContainer,
 } from "./restaurants.screen.styles";
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info.component";
+import { Search } from "../components/search.component";
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
@@ -21,9 +20,7 @@ export const RestaurantsScreen = () => {
           <Loading size={50} animating={true} color="#2182BD" />
         </LoadingContainer>
       )}
-      <SearchContainer>
-        <Searchbar value="" />
-      </SearchContainer>
+      <Search />
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
