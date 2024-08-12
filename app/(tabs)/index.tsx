73bc894956e610528e938281949a3fa79f3ccc8c
LyyHua <lyhuavanly@gmail.com>
2024-08-12 23:31:32 +0700

    Yeah i just found out how to fix the stupid .env shit from stackoverflow thanks a lot GITHUB COPILOT EATSHIT BITCH

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,20 +7,25 @@ import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
 import { ThemeProvider } from "styled-components/native";
 import { RestaurantsNavigator } from "@/src/infrastructure/navigation/restaurants.navigator";
 import { initializeApp } from "firebase/app";
-import { config } from "dotenv";
-import { useState } from "react";
 import { AuthenticationContextProvider } from "@/src/services/authentication/authentication.context";
 import { FavouritesContextProvider } from "@/src/services/favorites/favorites.context";
-
-config();
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+// eslint-disable-next-line import/no-unresolved
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
